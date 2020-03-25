3a0abed0ea34b2c523c5f5d8fa041735d29418d9
teequa <61880781+teequa@users.noreply.github.com>
2020-03-25 02:34:43 +0100

    Added kick/ban commands

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const newUsers = new Discord.Collection();
+// const embedList = require('./embeds.js');
 
 const client = new Discord.Client();
 
@@ -8,9 +9,10 @@ client.on('ready', () => {
   console.log(`Teeq client is up and running!`);
 });
 
+//User join/leave
 client.on('guildMemberAdd', (member) => {
   const channel = member.guild.channels.cache.find(channel => channel.id === config.welcomeChannel);
-  const newUser = member.has(member.id, member.user);
+  //const newUser = member.has(member.id, member.user);
   if (!channel) return;
 
   if (member) {
@@ -36,6 +38,8 @@ client.on("message", message => {
 
   let arg = message.content.split(" ").slice(1);
 
+  let memRole = message.member.roles.cache;
+
   if (cmd === "welcome") {
     message.welcomeChannel.send('this is the welcome channel');
   }
@@ -60,14 +64,68 @@ client.on("message", message => {
     message.channel.send(`this is the role id for the moderator role: ${config.moderator}`);
   } else
 
-  if (cmd === "admin") {
-  message.channel.send(`The supreme leader is ${message.member.user}`);
-} else
-
-if (cmd === "invite") {
+  if (cmd === "invite") {
   message.channel.send(`invitation link: ${config.invite}`);
 }
 
+  if (cmd === "kick") {
+  if (!memRole.get(config.moderator))
+  return message.reply('You do not have permission to use this command');
+
+    const user = message.mentions.users.first();
+
+    if (user) {
+      const member = message.guild.member(user);
+
+      if (member) {
+        member.kick('Were kicked form the channel').then(()=> {
+          message.reply(`succsessfuly kicked ${user.tag}`);
+        }).catch(err => {
+          message.reply('unable to kick user');
+          console.log(err);
+        });
+      } else{
+        message.reply(`that user is not a member of this server`);
+      }
+    } else {
+      message.reply('you need to specify a user');
+    }
+  }
+
+  if (cmd === "ban") {
+
+    if (!memRole.get(config.moderator))
+
+  return message.reply('You do not have permission to use this command');
+
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+
+      if (member) {
+
+        const banEmbed = new Discord.MessageEmbed()
+        .setColor ('#ff0000')
+        .setTitle ('User banned')
+        .addFields (
+          {name: `${user.tag} banned`, value: 'The user was banned by infringment of the server rules'}
+        )
+        .addField (`Ban issued by:`, `${message.author.tag}`)
+        .setTimestamp()
+
+        member.ban().then(() => {
+          message.channel.send(banEmbed);
+        }).catch(err => {
+          message.reply('unable to ban user');
+          console.log(err);
+        });
+      } else{
+        message.reply(`that user is not a member of this server`);
+      }
+    } else {
+      message.reply('you need to specify a user');
+    }
+  }
 });
 
 client.login(config.token);
